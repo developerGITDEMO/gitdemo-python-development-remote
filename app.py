a2cf549dfa9db044581606eba42d1fead42d0f72
developerGITDEMO <chamomafud@gmail.com>
2022-01-13 04:20:08 -0600

    ADD:  Agregando la funcionalidad de saludoi

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ from flask.wrappers import Request
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
+#Saludo
+@app.route("/")
+def index():
+	flash("¿Cuál es tu nombre?")
+	return render_template("index.html")
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hola " + str(request.form['name_input']) + " !")
+	return render_template("index.html")
+
 HOST = "localhost"
 PORT = 4000
 DEBUG = True
